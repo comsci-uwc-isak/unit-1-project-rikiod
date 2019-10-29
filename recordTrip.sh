96bf7f0196c117940aa9d12a6d0c97bb23ba1ed4
rikiod <54606933+rikiod@users.noreply.github.com>
2019-10-29 16:51:02 +0900

    updated program

--- recordTrip.sh
+++ recordTrip.sh
@@ -12,15 +12,15 @@ licensePlate=$4
 
 if [[ $# -ne 4 ]]; then
 	echo "Sorry, there was an error. Next time, please enter the distance (km), the start date, the finish date, and the license plate."
-	exit
-else 
+  exit
+else
+  cd ../db
 	if [[ -f $licensePlate.txt ]]; then #Checking if the car exists or not
-		echo "$distance $startDate $finishData" >> RentalCarApp/db/maincarfile.txt
-		bash "The $distance km trip has been logged on the car with plate $licensePlate."
-		echo "To add another car, run this program with the arguments distance, start date, finish date, and license plate."
+		echo "$distance $startDate $finishDate" >> $licensePlate".txt"
+		echo "$distance $startDate $finishDate $licensePlate" >> trips.txt
+		echo "The $distance km trip has been logged on the car with plate $licensePlate."
 		exit
 	else
 		echo "Sorry, that car hasn't been created yet."
-		exit
 	fi
 fi
