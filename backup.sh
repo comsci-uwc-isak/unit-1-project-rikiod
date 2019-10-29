4737ecc71bcb19c65de8da3447158e86162c2ec3
rikiod <54606933+rikiod@users.noreply.github.com>
2019-10-29 16:49:19 +0900

    updated program

--- backup.sh
+++ backup.sh
@@ -3,11 +3,12 @@
 #This program will backup the entirity of the database folder within the MinimalCarRental application.
 
 location=$1
+
 if [[ $# -ne 1 ]]; then
-	echo "Sorry, there was an error."
+	echo "Sorry, there was an error with backing up your files."
 	exit
 else
-	cp -a RentalCarApp/db $location
+	cp -a /Users/rikiodahlgren/Desktop/RentalCarApp/db $location
+	echo "Successfully backed up into $location."
 fi
-
 
